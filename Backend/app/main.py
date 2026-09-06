1b908abbf058c982e45d3cdf36b320e09a019377
dineshpsingh16 <Dinesh.singh@stunm.in>
2026-09-06 23:31:46 +0530

    fix cors

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -19,7 +19,7 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://hiring-ai-outreach-and-worflow.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
